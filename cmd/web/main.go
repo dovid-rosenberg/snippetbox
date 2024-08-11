1a2bc89a677e84b9122feb09945adce86dda76ef
Dovid Rosenberg <dovid-rosenberg@outlook.com>
2024-08-11 15:44:10 -0400

    Add CL args
    Create cfg object from flags
    Use for addr and staticDir

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
+type config struct {
+	addr      string
+	staticDir string
+}
+
 func main() {
+	var cfg config
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+
+	flag.Parse()
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir(cfg.staticDir))
 
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("GET /{$}", home)
@@ -16,8 +27,8 @@ func main() {
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Print("starting server on :8080")
+	log.Printf("starting server on %s", cfg.addr)
 
-	err := http.ListenAndServe(":8080", mux)
+	err := http.ListenAndServe(cfg.addr, mux)
 	log.Fatal(err)
 }
