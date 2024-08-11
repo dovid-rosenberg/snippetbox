8f301c8f04bbcf51b9f37baee501c0ab0cdc3ac1
Dovid Rosenberg <dovid-rosenberg@outlook.com>
2024-08-11 14:58:27 -0400

    Static files
    Add static file server
    Add static files to main template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -23,7 +23,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	
+
 	err = ts.ExecuteTemplate(w, "base", nil)
 
 	if err != nil {
